300574b7c3fe539bdde3d12e923ee2b214c30d79
ashley Peresie <78498406+c55h72@users.noreply.github.com>
2021-03-11 16:26:05 -0600

    updated controls logic
    updated so that multiple keys register at the same time

--- www/js/index.js
+++ www/js/index.js
@@ -17,39 +17,81 @@ function setup(){
 
 
   //light
-  // light = new THREE.DirectionalLight(0xffffff, 0.5)
-  // light.position.set( 2, 2, 2 )
-  // light.rotation.set( Math.PI/2, 2, 2 )
-  // scene.add(light)
 
-  // const light = new THREE.HemisphereLight( 0xeeeeff, 0x777788, 0.75 );
-  // light.position.set( 0.5, 1, 0.75 );
-  // scene.add( light );
+  light = new THREE.DirectionalLight(0x9cf2ff, 10000)
+  light.position.set( 2, 2, 2 )
+  light.rotation.set( Math.PI/2, 2, 2 )
+  scene.add(light)
 
-  //
-  window.addEventListener('keydown', () => {
 
-    // console.log(event.keyCode)
-  if (event.keyCode === 87){
-    moveForward()
-  } else if (event.keyCode === 83) {
-    moveBackward()
-  } else if (event.keyCode === 65) {
-    moveLeft()
-  } else if (event.keyCode === 68) {
-    moveRight()
-  }
+  //controls
+  var keyMap = [];
+
+  window.addEventListener('keydown', (e)=>{
+      if(!keyMap.includes(e.keyCode)){
+          keyMap.push(e.keyCode);
+      }
+      checkGameKeys()
   })
-window.addEventListener('keydown', () => {
-  if (event.keyCode === 37) {
-      rotateLeft()
-  } else if (event.keyCode === 39) {
-      rotateRight()
-    }
-})
+  window.addEventListener('keyup', (e)=>{
+      if(keyMap.includes(e.keyCode)){
+          keyMap.splice(keyMap.indexOf(e.keyCode), 1);
+      }
+      checkGameKeys()
+      console.log(e.keyCode)
 
+  })
+  function key(x){
+      return (keyMap.includes(x));
+  }
 
+  function checkGameKeys(){
+      if(key(37)){
+          camera.rotation.y += Math.PI/8
+      }
+      if(key(39)){
+          camera.rotation.y -= Math.PI/8
+      }
+      if(key(65)){
+          camera.translateX(-1)
+      }
+      if(key(68)){
+          camera.translateX(1)
+      }
+      if(key(87)){
+          camera.translateZ(-1)
+      }
+      if(key(83)){
+          camera.translateZ(1)
+      }
+      if(key(32)){
+          camera.position.y += 1
+      }
+      if(key(16)){
+          camera.position.y -= 1
+      }
+  }
 
+            //   window.addEventListener('keydown', () => {
+            //
+            //     // console.log(event.keyCode)
+            //   if (event.keyCode === 87){
+            //     moveForward()
+            //   } else if (event.keyCode === 83) {
+            //     moveBackward()
+            //   } else if (event.keyCode === 65) {
+            //     moveLeft()
+            //   } else if (event.keyCode === 68) {
+            //     moveRight()
+            //   }
+            //   })
+            // window.addEventListener('keydown', () => {
+            //   if (event.keyCode === 37) {
+            //       rotateLeft()
+            //   } else if (event.keyCode === 39) {
+            //       rotateRight()
+            //     }
+            // })
 
 
 
@@ -58,22 +100,18 @@ window.addEventListener('keydown', () => {
     new THREE.MeshNormalMaterial())
   ground.rotation.x = - Math.PI / 2 // rotates X/Y to X/Z
   scene.add( ground )
+
+
   // gen terrain
   terrain = new TerrainGen(scene)
 
   cube = new RotatingCube(scene)
-
-
-
 
   //set up renderer **??** does this always have to be at the end?
   renderer = new THREE.WebGLRenderer()
   renderer.setSize(innerWidth, innerHeight)
   document.body.appendChild(renderer.domElement)
   window.addEventListener('resize', onWindowResize)
-
-
-
 
 }
 
@@ -86,26 +124,26 @@ function onWindowResize() {
 }
 
 
-
-function moveForward(){
-  camera.position.z -= 1
-}
-function moveBackward(){
-  camera.position.z += 1
-}
-function moveLeft(){
-  camera.position.x -= 1
-}
-function moveRight(){
-  camera.position.x += 1
-}
-
-function rotateLeft(){
-  camera.rotation.y += Math.PI/8
-}
-function rotateRight(){
-  camera.rotation.y -= Math.PI/8
-}
+              //
+              // function moveForward(){
+              //   camera.translateZ(-1)
+              // }
+              // function moveBackward(){
+              //   camera.translateZ(1)
+              // }
+              // function moveLeft(){
+              //   camera.translateX(-1)
+              // }
+              // function moveRight(){
+              //   camera.translateX(1)
+              // }
+              //
+              // function rotateLeft(){
+              //   camera.rotation.y += Math.PI/8
+              // }
+              // function rotateRight(){
+              //   camera.rotation.y -= Math.PI/8
+              // }
 
 
 
