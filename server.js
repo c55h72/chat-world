873c40624c75aa3598b2c2d4dfd709b5b50e3928
ashley Peresie <78498406+c55h72@users.noreply.github.com>
2021-03-10 19:44:36 -0600

    create server.js
    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const bodyParser = require('body-parser')
+const io = require('socket.io')(http)
+const restAPI = require('./my_modules/rest-api.js')
+const socketAPI = require('./my_modules/socket-api.js')
+const port = 8000
+
+app.use(bodyParser.json())
+app.use(restAPI)
+app.use(express.static('www'))
+
+io.on('connection', (socket) => socketAPI(socket, io))
+
+http.listen(port, (err) => {
+  if (err) return console.log(err)
+  console.log(`
+    listening on localhost:${port}
+    document root is ${__dirname}
+    ctrl c to quit
+    `)
+
+})
